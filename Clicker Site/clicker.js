73df18b20303c4144527167dcb372be32d66c69b
B-Hulse <b.hulse99@gmail.com>
2018-06-29 16:53:50 +0100

    Adding header image and formatting

--- Clicker Site/clicker.js
+++ Clicker Site/clicker.js
@@ -1,23 +1,28 @@
 var counter = 0;
-var time = 10;
+var time = 1000;
 var started = false;
 var timer;
+var times=[];
+var lastTime;
 document.getElementById("count").innerHTML = counter;
-document.getElementById("timer").innerHTML = "Time: " + time;
+document.getElementById("timer").innerHTML = "Time: " + (time/100);
 
-var clicked = function() {
+var clicked = function () {
 	if (!started) {
-	timer = setInterval(function() {
+		timer = setInterval(function () {
 			time--;
-			document.getElementById("timer").innerHTML = "Time: " + time;
-			if (time == 0){
-				document.getElementById("button").disabled=true;
-				document.getElementById("button").innerHTML="Time up";
+			document.getElementById("timer").innerHTML = "Time: " + (time/100);
+			if (time == 0) {
+				document.getElementById("button").disabled = true;
+				document.getElementById("button").innerHTML = "Time up";
+				console.log(times);
 				clearInterval(timer);
 			}
-		}, 1000);
+		}, 10);
 		started = true;
 	}
+	times.push(lastTime-time);
+	lastTime = time;
 	counter++;
 	document.getElementById("count").innerHTML = counter;
 }
@@ -27,12 +32,14 @@ var restart = function () {
 		if (time > 0) {
 			clearInterval(timer);
 		}
-		time = 10;
-		document.getElementById("timer").innerHTML = "Time: " + time;
+		time = 1000;
+		document.getElementById("timer").innerHTML = "Time: " + (time/1000);
 		started = false;
-		document.getElementById("button").disabled=false;
-				document.getElementById("button").innerHTML="Click Here";
+		document.getElementById("button").disabled = false;
+		document.getElementById("button").innerHTML = "Click Here";
 		counter = 0;
 		document.getElementById("count").innerHTML = counter;
+		times = [];
+		lastTime=0;
 	}
 }
