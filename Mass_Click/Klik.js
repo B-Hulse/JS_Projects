b16645683d2bfd6d0c8b83de9ae9eb07c283a75a
B-Hulse <B.Hulse99@gmail.com>
2018-07-06 13:19:27 +0100

    updating Website and clicker to incorporate larger numbers

--- Mass_Click/Klik.js
+++ Mass_Click/Klik.js
@@ -1,16 +1,45 @@
 counters = new Array;
-
-// Max = 1000000000000000
-
+counters[0] = 0;
+display = document.getElementById("counter");
+max_counter = 1000000000000000;
+//1000000000000000
+    var padding = "";
+    for (let x = 0;x < (""+max_counter).length - 1;x++){
+        padding+= "0";
+    }
 function clicked() {
     counters[0]++;
     for (let i = 0; counters[i] > 0; i++) {
-        if (counters[i] == 1000000000000000) {
+        if (counters[i] == max_counter) {
+            counters[i] = 0;
+            if (counters[i+1] === undefined)
+            {
+                counters[i+1] = 1;
+            }
+            else {
             counters[i+1]++;
+            }
         }
         else {
             break;
         }       
     }
-    
+    printCount();
+}
+
+function printCount() {
+    var i = 0;
+    var countStr = "";
+    for (; counters[i+1] != undefined; i++);
+    console.log(i);
+    for (;i >= 0; i--) {
+        if (counters[i+1] == undefined){
+            countStr += counters[i];
+        } else {
+        var str = "" + counters[i];
+        var cnt = padding.substring(0,padding.length - str.length) +str;
+        countStr+= cnt;
+        }    
+    }
+    display.innerHTML = countStr;
 }
